2df40394405fe3948f7246bb14efad1ff06b331a
Erick <santoserick9@gmail.com>
2023-01-01 19:18:40 -0300

    add either in response from usecase

--- src/domain/usecases/pokemon/load-url-list.ts
+++ src/domain/usecases/pokemon/load-url-list.ts
@@ -1,8 +1,13 @@
+import { UnexpectedError } from "@domain/errors";
+import { Either } from "@shared/helpers";
+
 export interface LoadUrlListRequest {
-  limit: number;
+  params: {
+    limit: number;
+  };
 }
 
-export type LoadUrlListResponse = string[];
+export type LoadUrlListResponse = Either<UnexpectedError, string[]>;
 
 export interface LoadUrlList {
   execute: (request: LoadUrlListRequest) => Promise<LoadUrlListResponse>;
