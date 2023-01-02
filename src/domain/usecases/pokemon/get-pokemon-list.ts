1cd01fff4d59c2f1deb55c7b0980589923da8962
Erick <santoserick9@gmail.com>
2023-01-02 16:55:34 -0300

    change name load to get

--- src/domain/usecases/pokemon/get-pokemon-list.ts
+++ src/domain/usecases/pokemon/get-pokemon-list.ts
@@ -1,10 +1,12 @@
 import { Pokemon } from "@domain/entities";
+import { UnexpectedError } from "@domain/errors";
+import { Either } from "@shared/helpers";
 
 export interface LoadPokemonListRequest {
   limit: number;
 }
 
-export type LoadPokemonListResponse = Pokemon[];
+export type LoadPokemonListResponse = Either<UnexpectedError, Pokemon[]>;
 
 export interface LoadPokemonList {
   execute: (
--- src/domain/usecases/pokemon/index.ts
+++ src/domain/usecases/pokemon/index.ts
@@ -1,3 +1,3 @@
-export * from "./load-pokemon-list";
+export * from "./get-pokemon-list";
 export * from "./get-pokemon-url-list";
 export * from "./get-pokemon";
