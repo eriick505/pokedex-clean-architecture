261ca5a1a400afb19ea4d09a386dfada6e2c73f3
Erick <santoserick9@gmail.com>
2023-01-04 17:39:17 -0300

    refact use case return

--- src/application/usecases/pokemon/remote-get-pokemon-name-list.ts
+++ src/application/usecases/pokemon/remote-get-pokemon-name-list.ts
@@ -1,14 +1,10 @@
-import {
+import type {
   GetPokemonNameList,
   GetPokemonNameListRequest,
   GetPokemonNameListResponse,
 } from "@domain/usecases/pokemon";
-import { UnexpectedError } from "@domain/errors";
 
 import { PokemonRepository } from "@application/repositories";
-import { HttpStatusCode } from "@application/protocols/http";
-
-import { left } from "@shared/helpers";
 
 export class RemoteGetPokemonNameList implements GetPokemonNameList {
   constructor(private readonly pokemonRepository: PokemonRepository) {}
@@ -16,13 +12,6 @@ export class RemoteGetPokemonNameList implements GetPokemonNameList {
   async execute(
     request: GetPokemonNameListRequest
   ): Promise<GetPokemonNameListResponse> {
-    const response = await this.pokemonRepository.getPokemonNameList(request);
-
-    switch (response.statusCode) {
-      case HttpStatusCode.ok:
-        return response.data;
-      default:
-        return left(new UnexpectedError());
-    }
+    return await this.pokemonRepository.getPokemonNameList(request);
   }
 }
