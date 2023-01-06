68e7590ae64ae04c1ea1114f56f8e47047e7776a
Erick <santoserick9@gmail.com>
2023-01-06 20:46:57 -0300

    update mapper interface to send data to domain

--- src/infra/repositories/axios/mappers/axios-specie-mapper.ts
+++ src/infra/repositories/axios/mappers/axios-specie-mapper.ts
@@ -1,8 +1,15 @@
 import { Specie } from "@domain/entities";
 import { RawSpecie } from "@infra/repositories/axios/model";
 
+interface RawSpecieToDoamin extends RawSpecie {
+  evolution_chain: {
+    url: string;
+    id: string | number;
+  };
+}
+
 export class AxiosSpecieMapper {
-  static toDomain(raw: RawSpecie): Specie {
+  static toDomain(raw: RawSpecieToDoamin): Specie {
     return Specie.create({
       color: raw.color.name,
       evolutionChain: raw.evolution_chain,
