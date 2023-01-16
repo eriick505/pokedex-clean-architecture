68922920487a15e0975c5d075e9dfd5f8ccab4af
Erick <santoserick9@gmail.com>
2023-01-16 17:10:58 -0300

    create header component

---
+++ src/infra/presentation/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { Menu } from "@presentation/components";
+
+export function Header() {
+  return (
+    <header className="rounded-xl px-10 shadow-lg bg-white">
+      <Menu />
+    </header>
+  );
+}
