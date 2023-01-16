e862de05de4f3fe436ca11ae66a1af28d37fa86f
Erick <santoserick9@gmail.com>
2023-01-15 21:57:43 -0300

    config path alias in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-macros", "babel-plugin-styled-components"],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@application": path.resolve(__dirname, "src/application"),
+      "@domain": path.resolve(__dirname, "src/domain"),
+      "@adapters": path.resolve(__dirname, "src/adapters"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@infra": path.resolve(__dirname, "src/infra"),
+      "@presentation": path.resolve(__dirname, "src/infra/presentation"),
+      "@test": path.resolve(__dirname, "test"),
+    },
+  },
+});
