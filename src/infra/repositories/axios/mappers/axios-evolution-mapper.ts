e7a70a1011774fc333a82313e238785fe6da0eaf
Erick <santoserick9@gmail.com>
2023-01-15 21:57:20 -0300

    update evolution mapper to handle undefined props

--- src/infra/repositories/axios/mappers/axios-evolution-mapper.ts
+++ src/infra/repositories/axios/mappers/axios-evolution-mapper.ts
@@ -4,10 +4,10 @@ import { EvolvesTo } from "@infra/repositories/axios/model";
 export class AxiosEvolutionMapper {
   static toDomain(raw: Omit<EvolvesTo, "evolves_to">): Evolution {
     return Evolution.create({
-      name: raw.species.name,
-      minLevel: raw.evolution_details[0].min_level ?? 1,
-      triggerName: raw.evolution_details[0].trigger.name ?? undefined,
-      item: raw.evolution_details[0].item.name ?? undefined,
+      name: raw.species?.name,
+      minLevel: raw.evolution_details[0]?.min_level ?? 1,
+      triggerName: raw.evolution_details[0]?.trigger.name ?? undefined,
+      item: raw.evolution_details[0]?.item?.name ?? undefined,
     });
   }
 }
